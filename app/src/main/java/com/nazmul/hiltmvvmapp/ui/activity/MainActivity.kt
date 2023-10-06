cc260dbde524112eadd3c578e55d1e08295760f2
nazmulcse247 <nazmulhoque247@gmail.com>
2023-10-07 02:50:07 +0600

    added base class

--- app/src/main/java/com/nazmul/hiltmvvmapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nazmul/hiltmvvmapp/ui/activity/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.nazmul.hiltmvvmapp.ui.activity
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.nazmul.hiltmvvmapp.R
 import com.nazmul.hiltmvvmapp.databinding.ActivityMainBinding
+import com.nazmul.hiltmvvmapp.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun viewBindingLayout(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+    override fun initializeView(savedInstanceState: Bundle?) {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
     }
+
 }
--- app/src/main/java/com/nazmul/hiltmvvmapp/ui/fragment/PopularTvShowFragment.kt
+++ app/src/main/java/com/nazmul/hiltmvvmapp/ui/fragment/PopularTvShowFragment.kt
@@ -1,15 +1,13 @@
 package com.nazmul.hiltmvvmapp.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.nazmul.hiltmvvmapp.common.Resource
 import com.nazmul.hiltmvvmapp.databinding.FragmentPopularTvShowBinding
+import com.nazmul.hiltmvvmapp.ui.base.BaseFragment
 import com.nazmul.hiltmvvmapp.viewmodel.MovieViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
@@ -18,27 +16,12 @@ import timber.log.Timber
 
 
 @AndroidEntryPoint
-class PopularTvShowFragment : Fragment() {
+class PopularTvShowFragment : BaseFragment<FragmentPopularTvShowBinding>() {
+    override fun viewBindingLayout(): FragmentPopularTvShowBinding = FragmentPopularTvShowBinding.inflate(layoutInflater)
 
-    private lateinit var binding: FragmentPopularTvShowBinding
     private val viewModel : MovieViewModel by viewModels()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentPopularTvShowBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun initializeView(savedInstanceState: Bundle?) {
         getPopularUIObserver()
     }
 
